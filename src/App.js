dc4dec1c52fc57b1e51559e05444fa115094b353
cste14 <emonnstu14@gmail.com>
2022-11-25 11:35:21 +0600

    file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Screen/Home";
+import Login from "./Screen/Login";
+import Register from "./Screen/Register";
+import Main from "./Layout/Main";
+import Profile from "./Screen/Profile";
+import ForgetPass from "./Screen/ForgetPass";
+import Admin from "./Admin/Admin";
+import Seller from "./User/Seller";
+import Buyer from "./User/Buyer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+
+          element: <Home />,
+        },
+      ],
+    },
+    { path: "*", element: <h1 className="text-center mt-5">Not Found!</h1> },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/Screen/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CarouselSlider from "../Component/Header/CarouselSlider";
+
+const Home = () => {
+  return (
+    <div>
+      <CarouselSlider />
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
